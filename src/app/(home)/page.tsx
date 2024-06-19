e989fc5f955d8cdbbb53489e9f986e63da7c1ad2
RafaGms <faelgomes2003@gmail.com>
2024-06-19 10:19:28 -0300

    feat: add welcome message

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,9 +1,17 @@
 import Header from "../_components/header";
-
+import { format } from 'date-fns'
+import { ptBR } from 'date-fns/locale'
 export default function Home() {
   return (
     <div>
       <Header />
+
+      <div className="mt-4 px-5">
+        <h2 className="text-xl font-bold">Olá Rafael</h2>
+        <p className="capitalize text-muted-foreground text-sm">{format(new Date(), "EEEE',' d 'de' yyyy", {
+          locale: ptBR
+        })}</p>
+      </div>
     </div>
   );
 }
